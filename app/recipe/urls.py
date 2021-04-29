5022a0fdabdc1802ce3b31f370a744949e0b41ae
Kacper Stasica <kacperstasica@yahoo.pl>
2021-04-29 12:34:19 +0200

    Re-factor tests and ingredients

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -4,8 +4,8 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register('tags', views.TagViewset)
-router.register('ingredients', views.IngredientViewset)
+router.register('tags', views.TagViewSet)
+router.register('ingredients', views.IngredientViewSet)
 
 app_name = 'recipe'
 
